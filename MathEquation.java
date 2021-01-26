dccb640049f990071fdc694f7fd11b3eb38b3c9f
Kennedy Wambua <kennywambu254@gmail.com>
2021-01-26 12:00:42 +0300

    java v1.0.0

---
+++ MathEquation.java
@@ -0,0 +1,25 @@
+package com.kennywambu.lessons;
+
+public class MathEquation {
+    double leftVal;
+    double rightVal;
+    char opCode;
+    double result;
+
+    void execute(){
+        switch (opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0 ? leftVal / rightVal : 0.0d;
+                break;
+        } 
+    }
+}
