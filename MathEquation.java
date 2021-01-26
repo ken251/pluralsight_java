efd89d9523c93045f624aa547216de12722ff50d
Kennedy Wambua <kennywambu254@gmail.com>
2021-01-26 13:43:42 +0300

    updating all

--- MathEquation.java
+++ MathEquation.java
@@ -6,6 +6,21 @@ public class MathEquation {
     char opCode;
     double result;
 
+    private  static  int numberOfCalculations;
+    private  static double sumOfResults;
+
+
+    public MathEquation(){}
+
+    public MathEquation(char opCode){
+        this.opCode = opCode;//initialised value of the opCode
+    }
+    public MathEquation(char opCode, double leftVal, double rightVal){
+        this(opCode);//chain a constructor
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
+
     void execute(){
         switch (opCode) {
             case 'a':
@@ -20,6 +35,28 @@ public class MathEquation {
             case 'd':
                 result = rightVal != 0 ? leftVal / rightVal : 0.0d;
                 break;
-        } 
+        }
+
+        numberOfCalculations++;
+        sumOfResults += result;
+    }
+    public  static double getAverageResults() {
+        return  sumOfResults/numberOfCalculations;
+    }
+
+    public double setLeftVal(double leftVal) {
+        this.leftVal = leftVal;
+        return leftVal;
+    }
+
+    public double setRightVal(double rightVal) {
+        this.rightVal = rightVal;
+        return rightVal;
+
+    }
+
+    public char setOpCode(char opCode) {
+        this.opCode = opCode;
+        return opCode;
     }
 }
